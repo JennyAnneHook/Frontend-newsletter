6f3e2c2bb8b55f6cc49260a3afea607c6beb4e12
Jenny Anne Höök <93882622+JennyAnneHook@users.noreply.github.com>
2022-05-29 22:05:55 +0200

    hej

--- src/components/loggedin.tsx
+++ src/components/loggedin.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export function LoggedIn() {
+
+  const [checked, setChecked] = useState(false)
+  console.log(checked);
+  
+
   return (
     <>
-      <h1>HEJ!</h1>;
+      <h1>HEJ!</h1>
+      <input type="checkbox" 
+      defaultChecked={checked}
+      onChange={() => setChecked(!checked)}/>
     </>
   );
 }
 
 export default LoggedIn;
+
