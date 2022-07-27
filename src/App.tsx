172a3aafa952f35ee335f57e97799861b81d2c87
Jenny Anne Höök <93882622+JennyAnneHook@users.noreply.github.com>
2022-07-27 19:46:37 +0200

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import axios from 'axios';
 import { request, ServerResponse } from 'http';
 import { Http2ServerRequest } from 'http2';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
@@ -15,7 +16,6 @@ function App() {
   
   const [newUser, setNewUser] = useState<InewUser>({username:'', password:'', subscribe:false})
   const [logginUser, setLoggin] = useState<InewUser>({username:'', password:'', subscribe:false})
-  
   
   // pushar till backend -> api
   function sendToApi(){
@@ -31,7 +31,7 @@ function App() {
 function handleChange(e:ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>){
   let name:string = e.target.name;
 
-  if(name == 'username' || name == 'password'){ 
+  if(name == 'username' || name == 'password'){
     setNewUser({...newUser, [name]: e.target.value});
 }
   else{
@@ -52,13 +52,18 @@ interface Iusers {
   const [error, setError] = useState(null);
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  
+  const navigate = useNavigate();
 
-  const handleLoggin = () =>{
+  const handleLoggin = (e: any) =>{
+    e.preventDefault();
     axios.post('http://localhost:3000/users/loggin', {
       username: username,
       password: password
     }).then(response => {
-      console.log('response', response);
+      navigate('/loggedin')
+      let GetPostData = response.data
+      return localStorage.setItem('_id', GetPostData._id)
     }).catch(error => {
       console.log('err', error);
     })
@@ -68,19 +73,17 @@ interface Iusers {
     <div className="App">
 
     <h1>Loggin</h1>
-
     <form>
       <label htmlFor="username">username</label><br />
       <input type="text" value={username} onChange={e => setUserName(e.target.value)} /><br />
       <label htmlFor="password">password</label><br />
       <input type="text" value={password} onChange={e => setPassword(e.target.value)} /><br />
-      <button onClick={handleLoggin}>skicka</button><br />
+      <button onClick={handleLoggin}>Logga in</button><br />
 
       {error && <p className='error'>{error}</p>}
     </form>
 
 <h1>Create User</h1>
-
     <form>
       <label htmlFor="username">user</label><br />
       <input type="text" name='username' onChange={handleChange}
@@ -93,106 +96,6 @@ interface Iusers {
       <button onClick={sendToApi}>skapa</button>
       </form>
     </div>
-    
-  );
-}
-
-export default App;import React, { ChangeEvent } from 'react';
-import './App.css';
-import { useState } from "react"
-import axios from 'axios';
-import { request, ServerResponse } from 'http';
-import { Http2ServerRequest } from 'http2';
-
-function App() {
-
-  interface InewUser {
-    username: String,
-    password: String,
-    subscribe: boolean
-  }
-  
-  const [newUser, setNewUser] = useState<InewUser>({username:'', password:'', subscribe:false})
-  const [logginUser, setLoggin] = useState<InewUser>({username:'', password:'', subscribe:false})
-  
-  
-  // pushar till backend -> api
-  function sendToApi(){
-    axios.post<InewUser[]>('http://localhost:3000/users/add', newUser)
-    .then(res =>{
-      console.log(res, 'Rätt');
-    }).catch(err =>{
-      console.log(err, 'Fel');
-    });
-  }
-
-  // använder oss av inputfällt i create user
-function handleChange(e:ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>){
-  let name:string = e.target.name;
-
-  if(name == 'username' || name == 'password'){ 
-    setNewUser({...newUser, [name]: e.target.value});
-}
-  else{
-    console.log('Det blev fel');
-}
-}
-
-// loggin funktion
-
-interface Iusers {
-    _id: string,
-    username: String,
-    password: String,
-    subscribe: boolean
-}
-
-  // testning av loggin (fungerar inte än)
-  const [error, setError] = useState(null);
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLoggin = () =>{
-    axios.post('http://localhost:3000/users/loggin', {
-      username: username,
-      password: password
-    }).then(response => {
-      console.log('response', response);
-    }).catch(error => {
-      console.log('err', error);
-    })
-  }
-
-  return (
-    <div className="App">
-
-    <h1>Loggin</h1>
-
-    <form>
-      <label htmlFor="username">username</label><br />
-      <input type="text" value={username} onChange={e => setUserName(e.target.value)} /><br />
-      <label htmlFor="password">password</label><br />
-      <input type="text" value={password} onChange={e => setPassword(e.target.value)} /><br />
-      <button onClick={handleLoggin}>skicka</button><br />
-
-      {error && <p className='error'>{error}</p>}
-    </form>
-
-<h1>Create User</h1>
-
-    <form>
-      <label htmlFor="username">user</label><br />
-      <input type="text" name='username' onChange={handleChange}
-      />
-      <br />
-      <label htmlFor="password">password</label><br />
-      <input type="text" name='password' onChange={handleChange} 
-      />
-      <br />
-      <button onClick={sendToApi}>skapa</button>
-      </form>
-    </div>
-    
   );
 }
 
